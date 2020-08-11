b1e6eb66da00cdd6a407ad697554a6bb1c37b977
Ivan.Yakovlev <ivan.yakovlev@pochtavip.com>
2020-08-11 19:21:02 +0300

    change

---
+++ precache-manifest.a24f8917d2a37d7e1d620a3b34acb204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b880e3f1c19b5c3826142d049a6184be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcda2a8844f5e03dd79b",
+    "url": "/static/css/main.b04ffdc1.chunk.css"
+  },
+  {
+    "revision": "d257dca3a3bc6f68e9c4",
+    "url": "/static/js/2.1fa05fa5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1fa05fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcda2a8844f5e03dd79b",
+    "url": "/static/js/main.f3b8b43f.chunk.js"
+  },
+  {
+    "revision": "3d3ede8595dd928f0364",
+    "url": "/static/js/runtime-main.15b1e7b2.js"
+  }
+]);
